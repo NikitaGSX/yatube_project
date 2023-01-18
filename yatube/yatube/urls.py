3938cdabf4b82a702b3770343cd7ed778dce2620
Дриманович Никита Витальевич <ndrimanovich@vtb.ru>
2023-01-18 12:05:07 +0300

    Роутинг запросов в url. Обучение по написанию url и views

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -17,5 +17,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include('post.urls')),
+    path('groups/', views.groups),
 ]
