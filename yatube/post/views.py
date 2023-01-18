5cb627a68e9fa41d21ac44b164e19cffab544089
Дриманович Никита Витальевич <ndrimanovich@vtb.ru>
2023-01-19 01:35:53 +0300

    Добавлены urls и view-функции

--- yatube/post/views.py
+++ yatube/post/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse('Самая главная страница ))')
+
+
+def group_posts(request, slug):
+    return HttpResponse(f'Группа под номером {slug}')
